672f74f1fb13300043401679655faaa855173a2a
gladysgit <gladys.landsd@gmail.com>
2022-06-19 00:42:10 +0800

    20220618

--- js/config.js
+++ js/config.js
@@ -169,7 +169,7 @@ var config = {
             alignment: 'center',
             title: 'Enjoy!',
             image: '',
-            description: 'Would you feel like giving it a try after this semester?',
+            description: 'Would you feel like giving it a try after this presentation?',
             mapAnimation: 'flyTo',
             location: {
                 center: [114.12911, 22.41206],
